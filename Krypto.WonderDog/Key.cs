4ca3bc305738d072eaac3d4752b7ff170d06e257
Jason Davis <jasondavis303@gmail.com>
2021-05-04 10:55:36 -0600

    Improve usablility of the Key class

--- Krypto.WonderDog/Key.cs
+++ Krypto.WonderDog/Key.cs
@@ -7,117 +7,158 @@ namespace Krypto.WonderDog
 {
     public class Key
     {
-        public Key()
-        {
-            Password = new byte[new Random().Next(16, 129)];
-            using var rand = new RNGCryptoServiceProvider();
-            rand.GetBytes(Password);
+        private byte[] _salt = new byte[8];
+        private byte[] _password;
+        private int _iterations = 1000;
+        private HashAlgorithmName _hashAlgorithm = HashAlgorithmName.SHA512;
 
-            GenerateSalt();
-        }
+        public Key() { }
 
-        public Key(string password)
-        {
-            if (string.IsNullOrWhiteSpace(password))
-                throw new ArgumentNullException(nameof(password));
+        public Key(string password) => SetPassword(password);
 
-            Password = Encoding.UTF8.GetBytes(password);
-            GenerateSalt();
-        }
-
-        public Key(byte[] password)
-        {
-            if (password == null || password.Length == 0)
-                throw new ArgumentNullException(nameof(password));
-
-            Password = password;
-            GenerateSalt();
-        }
+        public Key(byte[] password) => Password = password;
 
         public Key(string password, string salt)
         {
-            if (string.IsNullOrWhiteSpace(password))
-                throw new ArgumentNullException(nameof(password));
-
-            if (string.IsNullOrWhiteSpace(salt))
-                throw new ArgumentNullException(nameof(salt));
-
-            Password = Encoding.UTF8.GetBytes(password);
-            Salt = Encoding.UTF8.GetBytes(salt);
-            if (Salt.Length < 8)
-                throw new ArgumentException("Salt is not at least 8 bytes", nameof(salt));
+            SetPassword(password);
+            SetSalt(salt);
         }
 
         public Key(string password, byte[] salt)
         {
-            if (string.IsNullOrWhiteSpace(password))
-                throw new ArgumentNullException(nameof(password));
-
-            if (salt == null || salt.Length == 0)
-                throw new ArgumentNullException(nameof(salt));
-
-            Password = Encoding.UTF8.GetBytes(password);
+            SetPassword(password);
             Salt = salt;
-            if (Salt.Length < 8)
-                throw new ArgumentException("Salt is not at least 8 bytes", nameof(salt));
         }
 
         public Key(byte[] password, string salt)
         {
-            if (password == null || password.Length == 0)
-                throw new ArgumentNullException(nameof(password));
-
-            if (string.IsNullOrWhiteSpace(salt))
-                throw new ArgumentNullException(nameof(salt));
-
             Password = password;
-            Salt = Encoding.UTF8.GetBytes(salt);
-            if (Salt.Length < 8)
-                throw new ArgumentException("Salt is not at least 8 bytes", nameof(salt));
+            SetSalt(salt);
         }
 
         public Key(byte[] password, byte[] salt)
         {
-            if (password == null || password.Length == 0)
-                throw new ArgumentNullException(nameof(password));
-
-            if (salt == null || salt.Length == 0)
-                throw new ArgumentNullException(nameof(salt));
-
             Password = password;
             Salt = salt;
-            if (Salt.Length < 8)
-                throw new ArgumentException("Salt is not at least 8 bytes", nameof(salt));
         }
 
-
-        public byte[] Password { get; set; }
-        public byte[] Salt { get; set; }
-        public int Iterations { get; set; } = 1000;
-        public HashAlgorithmName Algorithm { get; set; } = HashAlgorithmName.SHA512;
-
-        private void GenerateSalt()
+        public void SetPassword(string password)
         {
-            Salt = new byte[new Random().Next(16, 129)];
-            using var rand = new RNGCryptoServiceProvider();
-            rand.GetBytes(Salt);
+            if (string.IsNullOrWhiteSpace(password))
+                throw new ArgumentNullException(nameof(password));
+
+            if (password.Length < 8)
+                throw new ArgumentException("Password must be at least 8 characters long", nameof(password));
+
+            Password = Encoding.UTF8.GetBytes(password);
         }
 
-        internal void SetKey(SymmetricAlgorithm alg)
+        public void SetSalt(string salt)
         {
-            alg.Key = DeriveKey(alg.LegalKeySizes.Max(item => item.MaxSize));
+            if (salt == null)
+                throw new ArgumentNullException(nameof(salt));
+
+            if (salt.Length < 8)
+                throw new ArgumentException("Salt must be at least 8 characters long", nameof(salt));
+
+            Salt = Encoding.UTF8.GetBytes(salt);
         }
 
-        public byte[] DeriveKey(int bits)
+        public string GetPasswordString() => Encoding.UTF8.GetString(Password);
+
+        public string GetSaltString() => Encoding.UTF8.GetString(Salt);
+
+        /// <summary>
+        /// Default is 8 bytes, all zero. It is recommended to set and store the Salt value
+        /// </summary>
+        public byte[] Salt
         {
-            using var gen = new Rfc2898DeriveBytes(Password, Salt, Iterations, Algorithm);
-            return gen.GetBytes(bits / 8);
+            get => _salt;
+            set
+            {
+                if (value == null)
+                    throw new Exception("Salt may not be null");
+                if (value.Length < 8)
+                    throw new Exception("Salt must be at least 8 bytes long");
+                _salt = value;
+            }
+        }
+
+        public byte[] Password
+        {
+            get => _password;
+            set
+            {
+                if (value == null)
+                    throw new Exception("Password may not be null");
+                if (value.Length < 8)
+                    throw new Exception("Password must be at least 8 bytes long");
+                _password = value;
+            }
+        }
+
+        public int Iterations
+        {
+            get => _iterations;
+            set
+            {
+                if (value < 1)
+                    throw new Exception("Iterations may not be less than 1");
+                _iterations = value;
+            }
+        }
+
+        public HashAlgorithmName Algorithm
+        {
+            get => _hashAlgorithm;
+            set
+            {
+                if (value == null)
+                    throw new Exception("Algorithm may not be null");
+                _hashAlgorithm = value;
+            }
         }
 
         /// <summary>
-        /// Derives a 256 bit key
+        /// Sets the <see cref="Salt"/> to random, with a random length betweeen 256 and 2048 bits
         /// </summary>
-        /// <returns></returns>
-        public byte[] DeriveKey() => DeriveKey(256);
+        public void GenerateRandomSalt() => Salt = GenerateRandom();
+
+
+        /// <summary>
+        /// Sets the <see cref="Password"/> to random, with a random length betweeen 256 and 2048 bits
+        /// </summary>
+        public void GenerateRandomPassword() => Password = GenerateRandom();
+
+        private byte[] GenerateRandom()
+        {
+            //Generate a random byte array of random length between 32 and 256 bytes
+            var ret = new byte[new Random().Next(32, 257)];
+            using var rand = new RNGCryptoServiceProvider();
+            rand.GetBytes(ret);
+            return ret;
+        }
+
+        /// <summary>
+        /// Sets the key on the specified algorithm
+        /// </summary>
+        internal void SetKey(SymmetricAlgorithm alg) => alg.Key = DeriveKey(alg.LegalKeySizes.Max(item => item.MaxSize));
+
+
+        /// <summary>
+        /// Derives a Rfc2898 key from the <see cref="Password"/> and <see cref="Salt"/>
+        /// </summary>
+        /// <param name="size">Size in bits of the derived key. Must be a factor of 8</param>
+        public byte[] DeriveKey(int size = 256)
+        {
+            if (Password == null)
+                throw new Exception("Password has not been set");
+
+            if (size % 8 != 0)
+                throw new ArgumentException("Size is not a factor of 8", nameof(size));
+
+            using var gen = new Rfc2898DeriveBytes(Password, Salt, Iterations, Algorithm);
+            return gen.GetBytes(size / 8);
+        }
     }
 }
